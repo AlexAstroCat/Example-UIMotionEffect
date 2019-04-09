6fcbf673f98ded8d7f895883e615b1fcdde3bf82
Mike Nachbaur <mike@nachbaur.com>
2019-04-09 13:51:16 -0700

    Initial Commit

---
+++ Motion Shadows Demo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Motion Shadows Demo
+//
+//  Created by Michael Nachbaur on 2019-04-09.
+//  Copyright © 2019 Michael Nachbaur. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
