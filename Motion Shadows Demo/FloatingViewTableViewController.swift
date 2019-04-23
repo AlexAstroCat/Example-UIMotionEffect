6c474c0f332f98bacb0a60ec92c6248fafc6cdf6
Mike Nachbaur <mike@nachbaur.com>
2019-04-23 15:37:51 -0700

    Small tweaks

--- Motion Shadows Demo/FloatingViewTableViewController.swift
+++ Motion Shadows Demo/FloatingViewTableViewController.swift
@@ -10,18 +10,6 @@ import UIKit
 
 class FloatingViewTableViewController: UIViewController, UITableViewDataSource {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem
-    }
-
-    // MARK: - Table view data source
-
     func numberOfSections(in tableView: UITableView) -> Int {
         return 1
     }
